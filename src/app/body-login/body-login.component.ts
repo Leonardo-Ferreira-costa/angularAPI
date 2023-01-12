d1b9dec7565979a664f9f4e8b246a0726bd665c0
Leonardo-Ferreira-costa <62809356+Leonardo-Ferreira-costa@users.noreply.github.com>
2023-01-12 16:25:58 -0300

    Primeiro commit

---
+++ src/app/body-login/body-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body-login',
+  templateUrl: './body-login.component.html',
+  styleUrls: ['./body-login.component.css']
+})
+export class BodyLoginComponent {
+
+}
---
+++ src/app/menu-footer/menu-footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-footer',
+  templateUrl: './menu-footer.component.html',
+  styleUrls: ['./menu-footer.component.css']
+})
+export class MenuFooterComponent {
+  titleMenu = 'Brand';
+}
